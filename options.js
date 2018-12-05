9ff2fb17f5d77faf8b412fc0abe9fa0e9b826862
Patrick Kelly <patrick@pkelly.co>
2018-12-05 14:20:05 +0000

    added html and js to allow address data to be customised

--- options.js
+++ options.js
@@ -13,15 +13,23 @@ ga('send', 'pageview', '/options.html');
 // Saves options to chrome.storage
 function save_options() {
   var emailToFillWith = document.getElementById('emailToFillWith').value;
+  var addressLine1ToFillWith = document.getElementById('addressLine1ToFillWith').value;
+  var addressLine2ToFillWith = document.getElementById('addressLine2ToFillWith').value;
+  var cityToFillWith = document.getElementById('cityToFillWith').value;
+  var postcodeToFillWith = document.getElementById('postcodeToFillWith').value;
   chrome.storage.local.set({
-    email: emailToFillWith
+    email: emailToFillWith,
+    addressLine1: addressLine1ToFillWith,
+    addressLine2: addressLine2ToFillWith,
+    city: cityToFillWith,
+    postcode: postcodeToFillWith
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
+    status.textContent = 'Saved';
     setTimeout(function() {
       status.textContent = '';
-    }, 750);
+    }, 1500);
   });
 }
 
@@ -30,9 +38,17 @@ function save_options() {
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.local.get({
-    email: 'Enter_Your_Email@default.com'
+    email: 'Enter_Your_Email@default.com',
+    addressLine1: 'Buckingham Palace',
+    addressLine2: '',
+    city: 'London',
+    postcode: 'SW1A 1AA'
   }, function(items) {
     document.getElementById('emailToFillWith').value = items.email;
+    document.getElementById('addressLine1ToFillWith').value = items.addressLine1;
+    document.getElementById('addressLine2ToFillWith').value = items.addressLine2;
+    document.getElementById('cityToFillWith').value = items.city;
+    document.getElementById('postcodeToFillWith').value = items.postcode;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
